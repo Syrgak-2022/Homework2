07c81e499ca3f13a57f59f19025b61218ba346c4
Сыргак Иманкулов <syrgakimankulov@gmail.com>
2022-08-16 00:02:13 +0300

    Add homework 2

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+// Console.Write("Введите номер дня недели: ");
+// int dayNumber = Convert.ToInt32(Console.ReadLine());
+// if(dayNumber > 5)
+// {
+//     Console.WriteLine("Выходной день недели");
+// }
+// else Console.WriteLine("Рабочий день недели");
+
+
+
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа
+
+// Console.Write("Введите трехзначное число: ");
+// int user_input;
+// user_input = Convert.ToInt32(Console.ReadLine());
+// int number = user_input / 10;
+// int second = number % 10;
+// Console.WriteLine($"Вторая цифра из числа {user_input} равна {second} ");
